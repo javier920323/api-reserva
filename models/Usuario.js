99812efd3ccac79673b17ec243a4883e83e3959d
javier920323 <javiers920323@gmail.com>
2025-02-27 09:13:55 -0300

    Agregando modelo Usuario a la base de datos

---
+++ models/Usuario.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const UsuarioSchema = new mongoose.Schema({
+  nombre: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  rol: { type: String, enum: ["admin", "usuario"], default: "usuario" },
+});
+
+module.exports = mongoose.model("Usuario", UsuarioSchema);
